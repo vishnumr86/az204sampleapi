f965e1e53b4aa05316f74f38bd381f58d106d9bc
vishnumr86 <69620052+vishnumr86@users.noreply.github.com>
2025-10-30 21:17:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var app = express();
 
-app.listen(3000, () => {
- console.log("Server running on port 3000");
-});
+
+const PORT = process.env.PORT || 3000; 
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 app.get("/url", (req, res, next) => {
  res.json(["Tony","Lisa","Michael","Ginger","Food"]);
