e8d3a5135b698f8bf01a66cbc451058d5f161e6e
vishnumr86 <69620052+vishnumr86@users.noreply.github.com>
2025-11-19 15:06:48 +0530

    Port change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require("express");
 var app = express();
 
 
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 8000; 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 app.get("/url", (req, res, next) => {
